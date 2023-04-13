3e7e01d75829a5f76b6d55d14682d53c1ec22c64
JULIAVALERIO <juliav286@gmail.com>
2023-04-13 08:08:05 -0300

    concluinso hello world

--- src/main/java/com/generation/helloword/controller/HelloController.java
+++ src/main/java/com/generation/helloword/controller/HelloController.java
@@ -12,5 +12,26 @@ public class HelloController {
 	public String hello(){
 		return "<i>Hello Turma 62!</i>";
 		}
-
+	
+	@GetMapping("/bsm")
+	public String bsm() {
+		return "<b>Responsabilidade Pessoal<br />"
+				+ "Mentalidade de Crescimento<br />"
+				+ "Orientação ao Futuro<br />"
+				+ "Persistência<br />"
+				+ "Comunicação<br />"
+				+ "Orientação aos Detalhes<br />"
+				+ "Trabalho em Equipe<br />"
+				+ "Proatividade</b>";
+	}
+	
+	@GetMapping("/objetivos")
+	public String objetivos() {
+		return "<b>Objetivos<br />"
+				+ "- Melhorar minha diciplina<br />"
+				+ "- Melhorar meu foco<br />"
+				+ "- Participar mais das aulas<br />"
+				+ "- Apender o spring<br />"
+				+ "- Revisar os conceitos de Sql</b>";
+	}
  }
